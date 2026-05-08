d6e8c4e33433ede826e1d428c3d25619b49e353a
Chars Volta <charsvolta@gmail.com>
2026-05-08 01:16:14 -0300

    RGAWER

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ hiddenElements.forEach(el => {
   observer.observe(el);
 });
 
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+
+  let current = "";
+
+  sections.forEach(section => {
+
+    const sectionTop = section.offsetTop - 150;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+
+  });
+
+});
