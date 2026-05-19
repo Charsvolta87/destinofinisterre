e6802fc7ca1bc956092c99d132d17ef72ee4dcd9
Chars Volta <charsvolta@gmail.com>
2026-05-19 02:20:24 -0300

    ascac

--- js/eventos.js
+++ js/eventos.js
@@ -2,11 +2,21 @@ const eventos = [
 
   {
   nombre: "La Camencha",
-  fecha: "16·05·2026",
+  fecha: "16/05/2026",
   carpeta: "La Camencha 16 05 2026",
 
   fotos: [
-    "imagenes/La Camencha 16 05 2026/C:\fakepath\Destino_Finisterre_La_Camencha_Glew_01.jpg"
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_01.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_02.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_03.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_04.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_05.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_06.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_07.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_08.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_09.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_10.jpg",
+    "imagenes/La Camencha 16 05 2026/Destino_Finisterre_La_Camencha_Glew_11.jpg"
   ],
 
   videos: [
